cf289cdf21b00b40533dc24b122751fcc24c0072
jagdeep.singh <jagdeep.singh@infogain.com>
2016-08-25 16:46:30 +0530

    Log

--- app/src/main/java/com/example/jagdeepsingh/samplepro/MainActivity.java
+++ app/src/main/java/com/example/jagdeepsingh/samplepro/MainActivity.java
@@ -2,18 +2,21 @@ package com.example.jagdeepsingh.samplepro;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        Log.i(TAG, "onCreate: ");
 
         ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
     }
